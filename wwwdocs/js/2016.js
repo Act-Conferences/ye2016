3480a006e326cbff6e53c888fc750ff3690302db
Dan Fumarel <dan.fumarel@evozon.com>
2016-05-09 11:37:28 +0200

    add carousel homepage skelet

--- wwwdocs/js/2016.js
+++ wwwdocs/js/2016.js
@@ -14,33 +14,6 @@ $(document).ready(function() {
     i++;
   }, 3000)
 
-var end = new Date('08/24/2016 11:59 PM');
-
-    var _second = 1000;
-    var _minute = _second * 60;
-    var _hour = _minute * 60;
-    var _day = _hour * 24;
-    var timer;
-
-    function showRemaining() {
-        var now = new Date();
-        var distance = end - now;
-        if (distance < 0) {
-
-            clearInterval(timer);
-            document.getElementById('counter').innerHTML = 'Start';
-
-            return;
-        }
-        var days = Math.floor(distance / _day);
-        var hours = Math.floor((distance % _day) / _hour);
-        var minutes = Math.floor((distance % _hour) / _minute);
-        var seconds = Math.floor((distance % _minute) / _second);
-
-        document.getElementById('counter').innerHTML = days;
-    }
-
-    timer = setInterval(showRemaining, 1000);
 
 });
 
