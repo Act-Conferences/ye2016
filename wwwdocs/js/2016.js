dfa12acb36eee5faf92072bceb801d76f84f3ebd
Dan Fumarel <dan.fumarel@evozon.com>
2016-05-12 12:28:53 +0200

    add days in logo

--- wwwdocs/js/2016.js
+++ wwwdocs/js/2016.js
@@ -16,6 +16,21 @@ $(document).ready(function() {
     $('.changesjs').css('padding', '0');
   }
 
+  var i=0;
+   setInterval(function(){
+     if(i%2 == 0)
+         $('.third').fadeOut('slow', function(){
+             $('.toggleDays').fadeIn('slow')
+         })
+     else
+         $('.toggleDays').fadeOut('slow', function(){
+             $('.third').fadeIn('slow')
+         })    
+     i++;
+   }, 3000)
+
+
+
   $(window).resize(function(){
        $('.fill').css('height',$(".wrapper").outerHeight(true) - 94);
        $('.position').css('height', $(".wrapper").outerHeight(true) - $(".comming-soon").outerHeight(true));
