327550f800df8e9c94d70539603b1e809db2f638
Dan Fumarel <dan.fumarel@evozon.com>
2016-06-15 09:24:33 +0200

    footer on userInfo page

--- wwwdocs/js/2016.js
+++ wwwdocs/js/2016.js
@@ -93,9 +93,16 @@ $( "#confColapse" ).click(function() {
 
 //sticky footer on certain pages
 
+var windows_height = $(window).height();
+var current_height = windows_height-331;
+  $("#Statistics .verticalAl").css({'min-height' : current_height});
+  $("#Schedule .verticalAl").css({'min-height' : current_height});
+  $("#Statistics .verticalAl").css({'min-height' : current_height});
+  $("#Search .verticalAl").css({'min-height' : current_height});
+
   $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
   var target = $(e.target).attr("href") // activated tab
-  //console.log(target);
+  console.log(target);
   //console.log(typeof(target));
   if (target === "#Schedule") {
     var windows_height = $(window).height();
@@ -105,7 +112,15 @@ $( "#confColapse" ).click(function() {
     var windows_height = $(window).height();
     var current_height = windows_height-331;
     $("#Contact .verticalAl").css({'min-height' : current_height});
-  }
+  } else if (target === "#Statistics") {
+    var windows_height = $(window).height();
+    var current_height = windows_height-331;
+    $("#Statistics .verticalAl").css({'min-height' : current_height});
+  } else if (target === "#Search") {
+    var windows_height = $(window).height();
+    var current_height = windows_height-331;
+    $("#Search .verticalAl").css({'min-height' : current_height});
+  } 
 });
 
 
@@ -479,6 +494,13 @@ $(window).resize(function(){
        $('.ticketsBack').css('min-height',$(window).height() - 149);
        $('.userBackground').css('min-height',$(window).height() - 149);
 
+var windows_height = $(window).height();
+var current_height = windows_height-331;
+  $("#Statistics .verticalAl").css({'min-height' : current_height});
+  $("#Schedule .verticalAl").css({'min-height' : current_height});
+  $("#Statistics .verticalAl").css({'min-height' : current_height});
+  $("#Search .verticalAl").css({'min-height' : current_height});
+
 
 $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
   var target = $(e.target).attr("href") // activated tab
@@ -492,6 +514,16 @@ $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
     var windows_height = $(window).height();
     var current_height = windows_height-331;
     $("#Contact .verticalAl").css({'min-height' : current_height});
+  } else if (target === "#Statistics") {
+    console.log("hit");
+    var windows_height = $(window).height();
+    var current_height = windows_height-331;
+    $("#Statistics .verticalAl").css({'min-height' : current_height});
+  } else if (target === "#Search") {
+    console.log("hit");
+    var windows_height = $(window).height();
+    var current_height = windows_height-331;
+    $("#Search .verticalAl").css({'min-height' : current_height});
   }
 });
 
